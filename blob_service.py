b7206e0fc87e65513c7dce78d2f3cdac94f29433
AJ <11035621+axlj45@users.noreply.github.com>
2023-11-22 13:06:08 -0500

    feat(blob_service): added azure blob storage service for chart persistence

---
+++ blob_service.py
@@ -0,0 +1,35 @@
+from azure.storage.blob import BlobServiceClient
+from config import get_config
+import os
+
+
+cfg = get_config()
+blob_service_client = BlobServiceClient(account_url=cfg["az_blob_sas_url"])
+
+
+def upload_file_to_blob(container_name, file_path, blob_name):
+    """
+    Uploads a file to Azure Blob Storage
+    :param container_name: Name of the Azure Blob Storage container
+    :param file_path: Local path to the file
+    :param blob_name: Name of the blob (file name in storage)
+    """
+    try:
+        # Create a blob client using the local file name as the name for the blob
+        blob_client = blob_service_client.get_blob_client(
+            container=container_name, blob=blob_name
+        )
+
+        # Upload the file
+        with open(file_path, "rb") as data:
+            blob_client.upload_blob(data)
+
+        print(f"File {file_path} uploaded to {container_name}/{blob_name}")
+
+    except Exception as e:
+        print(e)
+
+
+def upload_chart(chart_path):
+    blob_name = os.path.basename(chart_path)
+    upload_file_to_blob(cfg["az_blob_container"], chart_path, blob_name)
